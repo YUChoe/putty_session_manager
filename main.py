c6f799921a885842c95e519ea65729cc1045e823
Yong-uk Choe <Yonguk.Choe@gmail.com>
2022-08-14 12:24:24 +0900

    Refactored as a Class

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import json
 import os
 from typing import Dict
 
+
 class App():
 
     config_filename: str = "ssh_session_config.json"
     config: Dict = {}
+    __NEW_HEIGHT = 0
 
     def __init__(self) -> None:
         self.load_config()
@@ -18,7 +20,8 @@ class App():
         # root.eval("tk::PlaceWindow . center")
         self.root.geometry(self.config['geometry'])
         self.root.resizable(False, False)
-
+        # https://stackoverflow.com/a/32289245
+        self.root.bind("<Configure>", lambda event, tkapproot=self.root: self.onWindowConfig(event, tkapproot))
         self.draw_frame()
 
     def run(self):
@@ -55,26 +58,23 @@ class App():
         btn.grid(sticky='we')
 
         REG_PATHS = [r"Software\9bis.com\KiTTY\Sessions", r'Software\SimonTatham\PuTTY\Sessions']
-        cnt = 0
+        # cnt = 0
         for idx, sess_name in self.get_sub_registry(REG_PATHS):
             if sess_name == 'Default%20Settings': continue
             # SESS_PATH = f"{REG_PATH}\{sess_name}"
             tk.Grid.columnconfigure(frame, idx, weight=1)
             sess_name = unquote(sess_name, encoding="cp949")
             btn = tk.Button(
-                        frame,
-                        text=sess_name, font=("Cascadia Code Light", 9),
-                        anchor="w",
-                        bg="#28393a", fg="white",
-                        cursor="hand2",
-                        activebackground="#badee2", activeforeground="black",
-                        )
+                frame,
+                text=sess_name, font=("Cascadia Code Light", 9),
+                anchor="w",
+                bg="#28393a", fg="white",
+                cursor="hand2",
+                activebackground="#badee2", activeforeground="black",
+            )
             btn.grid(sticky='we')
-            # C:\Users\yucho\Documents\apps\kitty-bin-0.76.0.8\kitty.exe -load "**OracleVM0"
             btn.bind("<Button-1>", lambda event, session=sess_name: self.run_session(event, session))
-            cnt += 1
-            # __LAST_ELEMENT_NAME = f"{idx}{sess_name}"
-        # set_element_count(cnt)
+            # cnt += 1
 
     def get_sub_registry(self, REG_PATHS):
         i = 0
@@ -83,6 +83,7 @@ class App():
             while True:
                 try:
                     name = winreg.EnumKey(registry_key, i)
+                    print(f"{REG_PATH}/{name}")
                     yield i, name
                     i += 1
                 except OSError as e:
@@ -96,31 +97,17 @@ class App():
         print(cmd)
         subprocess.Popen(cmd)  # detached
 
-
-
-def onWindowConfig(event, tkapproot):
-    # https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/event-handlers.html
-    global __LAST_ELEMENT_NAME
-    global __NEW_HEIGHT
-    if str(event.widget) != '.':
+    def onWindowConfig(self, event, tkapproot):
+        # https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/event-handlers.html
         # widget_name = event.widget.cget("text")
-        # if __LAST_ELEMENT_NAME and __LAST_ELEMENT_NAME == widget_name:
-        #     tkapproot.geometry(f'200x{event.y + event.height}')
-        # tkapproot.unbind("<Configure>")
-        # w, h, x, y = get_geometry(tkapproot)
-        if __NEW_HEIGHT <= (event.y + event.height):
-            print(f'changed to {__NEW_HEIGHT} > {event.y + event.height}')
-            tkapproot.geometry(f'200x{event.y + event.height}')
-            __NEW_HEIGHT = event.y + event.height
+        if str(event.widget) == '.': return
+        this_height = event.y + event.height
+        if self.__NEW_HEIGHT <= this_height:
+            print(f'changed to {self.__NEW_HEIGHT} > {this_height}')
+            tkapproot.geometry(f'{self.config["width"]}x{this_height}')
+            self.__NEW_HEIGHT = this_height
+
 
 if __name__ == '__main__':
     app = App()
     app.run()
-    exit()
-    # https://stackoverflow.com/a/32289245
-    # root.bind("<Configure>", lambda event, tkapproot=root: onWindowConfig(event, tkapproot))
-
-"""
-python -m nuitka --plugin-enable=tk-inter --mingw64 --windows-disable-console --onefile main.py -o puttysession.exe
-# Nuitka-Onefile:WARNING: Onefile mode cannot compress without 'zstandard' module installed.
-"""
